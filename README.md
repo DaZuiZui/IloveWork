acf1ec8f1a14fd34ea329c79b24295eb767fc288
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2025-07-31 16:59:05 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# I love Work idea IntelliJ Plugin
-This project is for entertainment only, work hard and we all have a good future
+ 
---
+++ src/main/java/com/example/helloworld/HelloWorldAction.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloWorldAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Messages.showInfoMessage(e.getProject(), "Hello World!", "Hello World Plugin");
+    }
+} 
